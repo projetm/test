16409d030d81ca79a1f718dfd5e8f96ca461cfff
maximef91080 <maximef91080@gmail.com>
2016-06-07 14:18:05 -0400

    enseignant

---
+++ createtable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE enseignant (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(20) NOT NULL,
+    prenom VARCHAR(20) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    bureau INT NOT NULL
